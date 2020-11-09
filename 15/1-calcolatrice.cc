a41e651464f8a361fb1228f11805e9cdfa927617
Giovanni De Toni <giovanni.det@gmail.com>
2020-11-09 16:34:27 +0100

    Fix calculator exercise.

--- 15/1-calcolatrice.cc
+++ 15/1-calcolatrice.cc
@@ -2,9 +2,9 @@
 #include <cstdlib>
 using namespace std;
 
-int calcola(int a, int b, char operazione)
+float calcola(float a, float b, char operazione)
 {
-  int risultato;
+  float risultato;
   switch (operazione) {
     case '+':
       risultato = a + b;
@@ -13,30 +13,46 @@ int calcola(int a, int b, char operazione)
       risultato = a - b;
       break;
     case '/':
+
+      // Controllo che la divisione si possa fare
+      if (b == 0) {
+        cout << "Stai cercando di dividere per 0!" << endl;
+        exit(0);
+      }
+
       risultato = a/b;
       break;
-    case '*':
+    case 'x':
       risultato = a*b;
       break;
     default:
-      risultato = 0;
+      cout << "L'operazioni inserita non è stata riconosciuta!" << endl;
+      exit(0);
   }
   return risultato;
 }
 
 int main(int argc, char * argv [])
 {
-  int a, b, risultato;
+  float a, b, risultato;
+  char operazione;
 
+  // Controllo che siano stati dati gli argomenti corretti
   if (argc != 4)
   {
     cout << "Usage: calcolatrice <a> <b> <operazione>" << endl;
     exit(0);
   }
 
-  a = atoi(argv[1]);
-  b = atoi(argv[2]);
-  risultato = calcola(a, b, argv[3][0]);
+  // Converto i due numeri dati in input in interi
+  a = atof(argv[1]);
+  b = atof(argv[2]);
+
+  // Per ottenere l'operazione, prendo solo il primo carattere
+  operazione = argv[3][0];
+
+  // Calcolo il risultato
+  risultato = calcola(a, b, operazione);
 
   cout << "Il risultato è " << risultato << endl;
 
