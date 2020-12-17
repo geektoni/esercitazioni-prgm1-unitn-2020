664f981493a975f542e6b6eee2771b60700c0096
StefanoBerlato <sb.berlatostefano@gmail.com>
2020-12-17 13:06:37 +0100

    update lab 25

--- 25/1-esercizio-2-appello-luglio-2013.cc
+++ 25/1-esercizio-2-appello-luglio-2013.cc
@@ -3,15 +3,16 @@
 using namespace std;
 
 void stampaArray(char v [], int dim);
-void ruotaArray(char v [], int dim, int rotazione);
+void ruotaArrayInefficiente(char v [], int dim, int rotazione);
 void ruotaArrayEfficiente(char v [], int dim, int rotazione);
+void ruotaArrayEfficienteSemplice(char v [], int dim, int rotazione);
 
 
 int main() {
 
     int j;
-    char v [] = {'A','B','C','D','E','F','G','H'};
-    const int dim = 8;
+    char v [] = {'A','B','C','D','E'};
+    const int dim = 5;
 
     cout << "Inserisci j: ";
     cin >> j;
@@ -19,7 +20,9 @@ int main() {
     j = j % dim;
 
     stampaArray(v, dim);
-    ruotaArrayEfficiente(v, dim, j);
+    ruotaArrayInefficiente(v, dim, j);
+    //ruotaArrayEfficienteConArray(v, dim, j);
+    //ruotaArrayEfficiente(v, dim, j);
     stampaArray(v, dim);
 
     return 0;
@@ -42,32 +45,32 @@ void ruotaArrayEfficiente(char v [], int dim, int rotazione) {
         int indiceElementoDaMuovere = rotazione + k;
         for (int i = 0 ; i < dim-1 && indiceElementoDaMuovere != k ; i++) {
             v[indicePostoVuoto] = v[indiceElementoDaMuovere];
-            cout << "muovo indice " << indiceElementoDaMuovere << " in indice " << indicePostoVuoto << endl;
             rotazioniEffettuate++;
             indicePostoVuoto = indiceElementoDaMuovere;
             indiceElementoDaMuovere = (indiceElementoDaMuovere + rotazione) % dim;
         }
         v[indicePostoVuoto] = temp;
-        cout << "muovo indice " << k << " in indice " << indicePostoVuoto << endl;
         rotazioniEffettuate++;
     }
 }
 
-// NOTA: SOLO CON NUMERI DISPARI
-void ruotaArrayEfficienteRotta(char v [], int dim, int rotazione) {
-    rotazione = rotazione % dim;
-    int temp = v[0];
-    int indicePostoVuoto = 0;
-    int indiceElementoDaMuovere = rotazione;
-    for (int i = 0 ; i < dim-1 ; i++) {
-        v[indicePostoVuoto] = v[indiceElementoDaMuovere];
-        indicePostoVuoto = indiceElementoDaMuovere;
-        indiceElementoDaMuovere = (indiceElementoDaMuovere + rotazione) % dim;
+
+void ruotaArrayEfficienteConArray(char v [], int dim, int rotazione) {
+    int * temp = new int[rotazione];
+    for (int i = 0 ; i < rotazione ; i++) {
+        temp[i] = v[i];
     }
-    v[indicePostoVuoto] = temp;
+    for (int i = 0 ; i < dim-rotazione ; i++) {
+        v[i] = v[rotazione+i];
+    }
+    for (int i = 0 ; i < rotazione ; i++) {
+        v[dim-rotazione+i] = temp[i];
+    }
+    delete [] temp;
 }
 
-void ruotaArray(char v [], int dim, int rotazione) {
+
+void ruotaArrayInefficiente(char v [], int dim, int rotazione) {
     int temp;
     for (int i = 0 ; i < rotazione ; i++) {
         temp = v[0];
